f4840c3b66e87c42c8cc45f8e67f03993fa0c5f4
Stephen Schools <stephen.schools>
2017-09-19 13:37:13 -0400

    initial add test working, create others

--- src/com/ironyard/ReversePolishCalc.java
+++ src/com/ironyard/ReversePolishCalc.java
@@ -5,18 +5,15 @@ public class ReversePolishCalc {
     private int stackTop = 0;
 
     private String[] tokens;
-
-    // The stack
+    
     private String[] stack;
 
     public double calculate(String input) {
 
         tokens = input.split(",");
 
-        // 2. Allocate a stack as big as the number of tokens
         stack = new String [tokens.length];
 
-        // 3. write the algorithm
         for(int i = 0; i < tokens.length; ++i) {
             if (tokens[i].equals("+")) {
                 double temp1 = pop();
@@ -25,29 +22,23 @@ public class ReversePolishCalc {
             } else {
                 push(tokens[i]);
             }
-
         }
-
-        // 4. return the result
         return pop();
     }
 
     private void push(String number) {
-        // push on the stack
         stack[stackTop] = number;
         stackTop += 1;
     }
 
     private void push(double d) {
-        // change the double to a string and then push it on the stack
         String temp = Double.toString(d);
         stack[stackTop] = temp;
         stackTop += 1;
     }
 
     private double pop() {
-        // remove the string from the top of the stack and convert it to a double and return it (double)
-        double result = Double.parseDouble(stack[stackTop]);
+        double result = Double.parseDouble(stack[stackTop - 1]);
         stackTop -= 1;
         return result;
     }
