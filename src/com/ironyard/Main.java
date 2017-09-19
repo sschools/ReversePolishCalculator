8f705e63b90850ac876abf38cf2dde0ce79389ad
Stephen Schools <stephen.schools>
2017-09-19 11:39:43 -0400

    initial commit, files set up

---
+++ src/com/ironyard/Main.java
@@ -0,0 +1,29 @@
+package com.ironyard;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ReversePolishCalc rpc = new ReversePolishCalc();
+
+        String rpnExpression = "2.5,4.8,+";
+        double expectedResult = 7.3;
+        double actualResult = rpc.calculate(rpnExpression);
+        checkResult(rpnExpression, expectedResult, actualResult);
+
+        // Write tests for the other operators (-, *, /)
+
+        // Uncomment this test which has many operators
+        // rpnExpression = "5,1,2,+,4,*,+,3,-";
+        // expectedResult = 14.0;
+        // actualResult = rpn.calculate(rpnExpression);
+        // checkResult(rpnExpression, expectedResult, actualResult);
+    }
+
+    private static void checkResult(String expression, double expected, double actual) {
+        if (expected == actual) {
+            System.out.println("SUCCESS: " + expression + " is " + expected);
+        } else {
+            System.out.println("ERROR: " + expression + " expected " + expected + " actual " + actual);
+        }
+    }
+}
