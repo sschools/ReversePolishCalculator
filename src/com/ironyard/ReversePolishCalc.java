8e580b1be2541f6b1f5ae1e800e3ff9c751c4ee7
Stephen Schools <stephen.schools>
2017-09-19 13:24:13 -0400

    code written but not working, debug

--- src/com/ironyard/ReversePolishCalc.java
+++ src/com/ironyard/ReversePolishCalc.java
@@ -1,9 +1,9 @@
 package com.ironyard;
 
 public class ReversePolishCalc {
-    // You'll need a variable here to keep track of the top of the stack
 
-    // The array of the input string split up
+    private int stackTop = 0;
+
     private String[] tokens;
 
     // The stack
@@ -11,15 +11,21 @@ public class ReversePolishCalc {
 
     public double calculate(String input) {
 
-        // 1. Use the String split method to split the string into tokens at the commas
-        tokens = ...
+        tokens = input.split(",");
 
         // 2. Allocate a stack as big as the number of tokens
-        stack = ...
+        stack = new String [tokens.length];
 
         // 3. write the algorithm
         for(int i = 0; i < tokens.length; ++i) {
-            // calls to push() and pop() and do the math here
+            if (tokens[i].equals("+")) {
+                double temp1 = pop();
+                double temp2 = pop();
+                push(temp1 + temp2);
+            } else {
+                push(tokens[i]);
+            }
+
         }
 
         // 4. return the result
@@ -28,13 +34,21 @@ public class ReversePolishCalc {
 
     private void push(String number) {
         // push on the stack
+        stack[stackTop] = number;
+        stackTop += 1;
     }
 
     private void push(double d) {
         // change the double to a string and then push it on the stack
+        String temp = Double.toString(d);
+        stack[stackTop] = temp;
+        stackTop += 1;
     }
 
     private double pop() {
-        // remove the string from the top of the stack and convert it to a double and return it
+        // remove the string from the top of the stack and convert it to a double and return it (double)
+        double result = Double.parseDouble(stack[stackTop]);
+        stackTop -= 1;
+        return result;
     }
 }
